71d44baf4f3d36aae2c5d5e369c4579a1bc9e5cd
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-11 18:10:15 +0100

    MovieDao interface added

---
+++ src/main/java/ba/unsa/etf/rpr/MovieDao.java
@@ -0,0 +1,51 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Date;
+import java.util.List;
+
+public interface MovieDao extends Dao<Movie>{
+    /**
+     * Returns all movies with given title
+     * @param title search based on this parameter
+     * @return list of movies
+     */
+    List<Movie> searchByTitle(String title);
+
+    /**
+     * Returns all movies with given main actor
+     * @param main_actor search based on this parameter
+     * @return list of movies
+     */
+    List<Movie> searchByMainActor(String main_actor);
+    /**
+     * Returns all movies released after given date
+     * @param date search based on this parameter
+     * @return list of movies
+     */
+    List<Movie> getMoviesReleasedAfter(Date date);
+    /**
+     * Returns all movies shorter than given number of minutes
+     * @param length movie length in minutes
+     * @return list of movies
+     */
+    List<Movie> getMoviesShorterThan(int length);
+    /**
+     * Returns all movies longer than given number of minutes
+     * @param length movie length in minutes
+     * @return list of movies
+     */
+    List<Movie> getMoviesLongerThan(int length);
+    /**
+     * Returns all movies cheaper than given price
+     * @param price price of movie
+     * @return list of movies
+     */
+    List<Movie> getMoviesCheaperThan(double price);
+    /**
+     * Returns all movies of given genre
+     * @param genre price of movie
+     * @return list of movies
+     */
+    List<Movie> searchByGenre(String genre);
+
+}
