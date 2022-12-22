58f53b4a1fb01f9eabb406ef7b1ace05d4f12f6e
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-22 22:09:50 +0100

    MovieException added

--- src/main/java/ba/unsa/etf/rpr/exceptions/MovieException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/MovieException.java
@@ -1,6 +1,11 @@
 package ba.unsa.etf.rpr.exceptions;
 
+import java.sql.SQLException;
+
 public class MovieException extends RuntimeException{
+    public MovieException(String message, SQLException e){
+        super(message);
+    }
     public MovieException(String message){
         super(message);
     }
