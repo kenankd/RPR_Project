fdb166abf5f9b4655817a8437e876bfd7c2a9c4b
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-23 14:18:32 +0100

    updated MovieDao

--- src/main/java/ba/unsa/etf/rpr/dao/MovieDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MovieDao.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Movie;
+import ba.unsa.etf.rpr.exceptions.MovieException;
 
 import java.util.Date;
 import java.util.List;
@@ -11,43 +12,43 @@ public interface MovieDao extends Dao<Movie> {
      * @param title search based on this parameter
      * @return list of movies
      */
-    List<Movie> searchByTitle(String title);
+    List<Movie> searchByTitle(String title) throws MovieException;
 
     /**
      * Returns all movies with given main actor
      * @param main_actor search based on this parameter
      * @return list of movies
      */
-    List<Movie> searchByMainActor(String main_actor);
+    List<Movie> searchByMainActor(String main_actor)throws MovieException;
     /**
      * Returns all movies released after given date
      * @param date search based on this parameter
      * @return list of movies
      */
-    List<Movie> getMoviesReleasedAfter(Date date);
+    List<Movie> getMoviesReleasedAfter(Date date)throws MovieException;
     /**
      * Returns all movies shorter than given number of minutes
      * @param length movie length in minutes
      * @return list of movies
      */
-    List<Movie> getMoviesShorterThan(int length);
+    List<Movie> getMoviesShorterThan(int length)throws MovieException;
     /**
      * Returns all movies longer than given number of minutes
      * @param length movie length in minutes
      * @return list of movies
      */
-    List<Movie> getMoviesLongerThan(int length);
+    List<Movie> getMoviesLongerThan(int length)throws MovieException;
     /**
      * Returns all movies cheaper than given price
      * @param price price of movie
      * @return list of movies
      */
-    List<Movie> getMoviesCheaperThan(double price);
+    List<Movie> getMoviesCheaperThan(double price)throws MovieException;
     /**
      * Returns all movies of given genre
      * @param genre price of movie
      * @return list of movies
      */
-    List<Movie> searchByGenre(String genre);
+    List<Movie> searchByGenre(String genre)throws MovieException;
 
 }
