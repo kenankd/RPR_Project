7c81365b406980795fecd0fecf7e63c3ff89cf17
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-23 19:42:48 +0100

    updated PurchaseDaoSQLImplementation

--- src/main/java/ba/unsa/etf/rpr/dao/PurchaseDaoSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PurchaseDaoSQLImplementation.java
@@ -1,136 +1,38 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.SingletonConnection;
+import ba.unsa.etf.rpr.domain.Customer;
+import ba.unsa.etf.rpr.domain.Movie;
 import ba.unsa.etf.rpr.domain.Purchase;
-import ba.unsa.etf.rpr.exceptions.ConnectionException;
+import ba.unsa.etf.rpr.exceptions.MovieException;
+
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.sql.Date;
+import java.util.*;
 
-public class PurchaseDaoSQLImplementation implements PurchaseDao{
+public class PurchaseDaoSQLImplementation extends AbstractDao<Purchase> implements PurchaseDao{
     private Connection connection;
     public PurchaseDaoSQLImplementation() {
-        try {
-            connection = SingletonConnection.getInstance();
-        } catch (ConnectionException e) {
-            e.printStackTrace();
-        }
+        super("Purchase");
     }
 
-    @Override
-    public Purchase getById(int id) {
-        try {
-            PreparedStatement s = connection.prepareStatement("select * from Purchase where purchase_id _= ?");
-            s.setInt(1, id);
-            ResultSet rs = s.executeQuery();
-            if (rs.next()) {
-                Purchase c = new Purchase();
-                c.setCustomer_id(rs.getInt("customer_id"));
-                c.setMovie_id(rs.getInt("movie_id"));
-                c.setDate_of_rent(rs.getDate("purchase_date"));
-                c.setRent_id(rs.getInt("rent_id"));
-                rs.close();
-                return c;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 
-    @Override
-    public Purchase add(Purchase item) {
-        String add = "INSERT INTO Purchase(customer_id,movie_id,purchase_date) VALUES(?,?,?)";
-        try{
-            PreparedStatement ps=this.connection.prepareStatement(add,Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1,item.getCustomer_id());
-            ps.setInt(2,item.getMovie_id());
-            ps.setDate(3, (java.sql.Date) item.getDate_of_rent());
-            ps.executeUpdate();
-            ResultSet rs=ps.getGeneratedKeys();
-            rs.next();
-            item.setCustomer_id(rs.getInt(1));
-            return item;
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public Purchase update(Purchase item) {
-        try{
-            PreparedStatement ps=connection.prepareStatement("UPDATE Purchase SET customer_id=?,movie_id=?,purchase_date=? where purchase_id=?");
-            ps.setInt(1,item.getCustomer_id());
-            ps.setInt(2,item.getMovie_id());
-            ps.setDate(3, (java.sql.Date) item.getDate_of_rent());
-            ps.setInt(4,item.getRent_id());
-            ps.executeUpdate();
-            return item;
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-
-        }
-        return null;
-    }
-
-    @Override
-    public void delete(int id) {
-        String delete = "DELETE FROM Purchase WHERE purchase_id = ?";
-        try {
-            PreparedStatement s = connection.prepareStatement(delete);
-            s.setInt(1, id);
-            s.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<Purchase> getAll() {
-        List<Purchase> lista = new ArrayList<>();
-        try {
-            PreparedStatement s = connection.prepareStatement("select * from Purchase");
-            ResultSet rs = s.executeQuery();
-            while (rs.next()) {
-                Purchase c = new Purchase();
-                c.setCustomer_id(rs.getInt("customer_id"));
-                c.setMovie_id(rs.getInt("movie_id"));
-                c.setDate_of_rent(rs.getDate("purchase_date"));
-                c.setRent_id(rs.getInt("rent_id"));
-                lista.add(c);
-            }
-            rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return lista;
-    }
-
+/*
     @Override
     public List<Purchase> getByMovieId(int movie_id) {
-        List<Purchase> lista = new ArrayList<>();
+        List<Purchase> list = new ArrayList<>();
         try {
-            PreparedStatement s = connection.prepareStatement("select * from Purchase where movie_id= ?");
-            s.setInt(1,movie_id);
+            PreparedStatement s = getConnection().prepareStatement("select * from Purchase where title LIKE concat('%',?,'%')");
+            s.setString(1, title);
             ResultSet rs = s.executeQuery();
             while (rs.next()) {
-                Purchase c = new Purchase();
-                c.setCustomer_id(rs.getInt("customer_id"));
-                c.setMovie_id(rs.getInt("movie_id"));
-                c.setDate_of_rent(rs.getDate("purchase_date"));
-                c.setRent_id(rs.getInt("rent_id"));
-                lista.add(c);
+                list.add(row2object(rs));
             }
             rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return lista;
+        return list;
     }
 
     @Override
@@ -154,26 +56,81 @@ public class PurchaseDaoSQLImplementation implements PurchaseDao{
         }
         return lista;
     }
-
+*/
     @Override
-    public List<Purchase> getByDate(Date date) {
-        List<Purchase> lista = new ArrayList<>();
+    public List<Purchase> getByMovie(Movie movie) throws MovieException {
+        List<Purchase> list = new ArrayList<>();
         try {
-            PreparedStatement s = connection.prepareStatement("select * from Purchase where purchase_date= ?");
-            s.setDate(1, (java.sql.Date) date);
+            PreparedStatement s = getConnection().prepareStatement("select * from Purchase where movie = ?");
+            s.setInt(1, movie.getId());
             ResultSet rs = s.executeQuery();
             while (rs.next()) {
-                Purchase c = new Purchase();
-                c.setCustomer_id(rs.getInt("customer_id"));
-                c.setMovie_id(rs.getInt("movie_id"));
-                c.setDate_of_rent(rs.getDate("purchase_date"));
-                c.setRent_id(rs.getInt("rent_id"));
-                lista.add(c);
+                list.add(row2object(rs));
             }
             rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return lista;
+        return list;
+    }
+
+    @Override
+    public List<Purchase> getByCustomer(Customer customer) throws MovieException {
+        List<Purchase> list = new ArrayList<>();
+        try {
+            PreparedStatement s = getConnection().prepareStatement("select * from Purchase where customer = ?");
+            s.setInt(1, customer.getId());
+            ResultSet rs = s.executeQuery();
+            while (rs.next()) {
+                list.add(row2object(rs));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public List<Purchase> getByDate(java.util.Date date) throws MovieException {
+        List<Purchase> list = new ArrayList<>();
+        try {
+            PreparedStatement s = getConnection().prepareStatement("select * from Purchase where date_of_rent = ?");
+            s.setDate(1, (java.sql.Date) date);
+            ResultSet rs = s.executeQuery();
+            while (rs.next()) {
+                list.add(row2object(rs));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+
+    @Override
+    public Purchase row2object(ResultSet rs) throws MovieException {
+        try{
+            Purchase p = new Purchase();
+            p.setId(rs.getInt("id"));
+            p.setCustomer(DaoFactory.customerDao().getById(rs.getInt("customer")));
+            p.setMovie(DaoFactory.movieDao().getById(rs.getInt("movie")));
+            p.setDate_of_rent(rs.getDate("date_of_rent"));
+            return p;
+        } catch (SQLException e) {
+            throw new MovieException(e.getMessage(),e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Purchase object) {
+        Map<String, Object> map = new TreeMap<>();
+        map.put("id", object.getId());
+        map.put("movie", object.getMovie().getId());
+        map.put("date_of_rent", object.getDate_of_rent());
+        map.put("customer",object.getCustomer().getId());
+        return map;
     }
 }
