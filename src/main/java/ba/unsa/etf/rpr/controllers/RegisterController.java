654e48b5e37d164af8ee55ed1bc4918edcaa9fb8
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-26 13:25:29 +0100

    added register.fxml

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+    public Button buttonRegister;
+    public TextField fieldName;
+    public TextField fieldSurname;
+    public TextField fieldMail;
+    public TextField fieldUsername;
+    public PasswordField fieldPassword;
+
+    public void Register(ActionEvent actionEvent) {
+
+
+    }
+}
