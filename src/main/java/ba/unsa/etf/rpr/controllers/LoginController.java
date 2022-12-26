6075f810895525148335165392326cccdb6dc604
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-26 02:41:31 +0100

    added dummy logincontroller

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,8 +1,21 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController {
     public TextField fieldUsername;
+    public PasswordField fieldPassword;
+    public void buttonLogIn(){
+        if(fieldUsername.getText().trim().isEmpty()){
+          //  Alert alert = new Alert(Alert.AlertType.WARNING);
+           // alert.setTitle("Warning Dialog");
+           // alert.setHeaderText("Look, a Warning Dialog");
+            //alert.setContentText("Careful with the next step!");
 
+            //alert.showAndWait();
+        }
+        System.out.println("namjesti mladog kapitena bolje igra"+ fieldPassword.getText());
+    }
 }
