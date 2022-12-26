04ed2e0abf801c1752396aaaba99f976cb0e3c27
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-26 02:46:26 +0100

    Revert "added dummy logincontroller"
    This reverts commit 6075f810

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,21 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController {
     public TextField fieldUsername;
-    public PasswordField fieldPassword;
-    public void buttonLogIn(){
 
-          //  Alert alert = new Alert(Alert.AlertType.WARNING);
-           // alert.setTitle("Warning Dialog");
-           // alert.setHeaderText("Look, a Warning Dialog");
-            //alert.setContentText("Careful with the next step!");
-
-            //alert.showAndWait();
-
-      //  System.out.println("namjesti mladog kapitena bolje igra"+ fieldPassword.getText());
-    }
 }
