a7cd6e3d4829ae23fad627b833ece874f8d3d954
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-26 23:23:36 +0100

    implemented method to showAlert in ControllerHelper

--- src/main/java/ba/unsa/etf/rpr/controllers/ControllerHelper.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ControllerHelper.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Alert;
+
 public class ControllerHelper {
+    public static void showAlert(String title, String header, String content){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.show();
+    }
 }
