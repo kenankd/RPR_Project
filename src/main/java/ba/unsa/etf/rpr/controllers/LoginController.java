b32910297e8a18df3a2c0242e8c0ec33f19ee8cf
Kenan <kdizdarevi1@etf.unsa.ba>
2022-12-26 13:25:46 +0100

    updated logincontroller

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,8 +1,24 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+
 
 public class LoginController {
     public TextField fieldUsername;
+    public PasswordField fieldPassword;
 
+    public void buttonLogIn(ActionEvent actionevent) {
+
+        /*if (fieldUsername.getText().trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("I have a great message for you!");
+            alert.showAndWait();
+        }*/
+    }
 }
