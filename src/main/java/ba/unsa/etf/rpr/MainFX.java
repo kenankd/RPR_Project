e67d4b140c43cca5d20f34a799a1f0dc68956d39
Kenan <kdizdarevi1@etf.unsa.ba>
2023-01-15 23:46:27 +0100

    added home.fxml

--- src/main/java/ba/unsa/etf/rpr/MainFX.java
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -6,13 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class MainFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root= FXMLLoader.load(getClass().getResource("/resources/login.fxml"));
         stage.setTitle("Hello, World!");
-        stage.setScene(new Scene(root,200,200));
+        stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        stage.getScene().getStylesheets().add
+                (getClass().getResource("login.css").toExternalForm());
         stage.show();
     }
 
