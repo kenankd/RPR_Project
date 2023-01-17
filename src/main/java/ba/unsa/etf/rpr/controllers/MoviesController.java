5ceb45feb32fd05c706ffb2689c86fdb794a61e6
Kenan <kdizdarevi1@etf.unsa.ba>
2023-01-17 01:23:21 +0100

    added fields for moviescontroller

--- src/main/java/ba/unsa/etf/rpr/controllers/MoviesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MoviesController.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class MoviesController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoviesController implements Initializable {
+    @FXML
+    public TableColumn column_title;
+    @FXML
+    public TableColumn column_mainactor;
+    @FXML
+    public TableColumn column_genre;
+    @FXML
+    public TableColumn column_date;
+    @FXML
+    public TableColumn column_length;
+    @FXML
+    public TableColumn column_price;
+    @FXML
+    private TableView customer_table;
+
+    /**
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
 }
