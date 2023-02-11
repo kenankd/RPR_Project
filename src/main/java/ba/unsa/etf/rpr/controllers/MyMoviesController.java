f70bd430f9d0ec2aba8106124ccce575d6bb73a9
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-11 18:22:35 +0100

    updated MyMoviesController

--- src/main/java/ba/unsa/etf/rpr/controllers/MyMoviesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MyMoviesController.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
 public class MyMoviesController {
+    public TableView myMoviesTable;
+
+    public void showHome(ActionEvent actionEvent) {
+    }
+
+    public void showMovies(ActionEvent actionEvent) {
+    }
 }
