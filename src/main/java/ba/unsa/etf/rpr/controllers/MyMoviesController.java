764e4520b512acb6b9e2cd2cc69e3b3c3d7b73d3
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-11 18:32:19 +0100

    added from mymovies to home and movies options

--- src/main/java/ba/unsa/etf/rpr/controllers/MyMoviesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MyMoviesController.java
@@ -1,14 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class MyMoviesController {
     public TableView myMoviesTable;
-
-    public void showHome(ActionEvent actionEvent) {
+    private void switchScene(String fxml) throws IOException {
+        Stage stage=(Stage) myMoviesTable.getScene().getWindow();
+        FXMLLoader fxmlloader=new FXMLLoader(getClass().getResource("/fxml/"+fxml+".fxml"));
+        Parent root = fxmlloader.load();
+        stage.setTitle("Home");
+        stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
     }
 
-    public void showMovies(ActionEvent actionEvent) {
+    public void showHome(ActionEvent actionEvent) throws IOException {
+        switchScene("home");
+    }
+
+    public void showMovies(ActionEvent actionEvent) throws IOException {
+        switchScene("movies");
     }
 }
