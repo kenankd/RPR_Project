ccb2dfc4d9241c67ae1afe545b7bc46aae4d962f
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-12 21:00:06 +0100

    implemented basic methods for PurchaseManager

--- src/main/java/ba/unsa/etf/rpr/business/PurchaseManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/PurchaseManager.java
@@ -1,4 +1,24 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Customer;
+import ba.unsa.etf.rpr.domain.Purchase;
+import ba.unsa.etf.rpr.exceptions.MovieException;
+
+import java.util.List;
+
 public class PurchaseManager {
+    public List<Purchase> getAll() throws MovieException {
+        return DaoFactory.purchaseDao().getAll();
+    }
+    public void delete(int id) throws MovieException {
+        DaoFactory.purchaseDao().delete(id);
+    }
+    public Purchase add(Purchase item) throws MovieException{
+        return DaoFactory.purchaseDao().add(item);
+    }
+    public Purchase update(Purchase item) throws MovieException{
+        return DaoFactory.purchaseDao().update(item);
+    }
+
 }
