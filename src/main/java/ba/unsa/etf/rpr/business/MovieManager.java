859bea71910f508abb276f6ed0239483ffe29368
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-12 21:14:43 +0100

    implemented methods in MovieManager

--- src/main/java/ba/unsa/etf/rpr/business/MovieManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/MovieManager.java
@@ -1,5 +1,23 @@
 package ba.unsa.etf.rpr.business;
 
-public class MovieManager {
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Movie;
+import ba.unsa.etf.rpr.domain.Purchase;
+import ba.unsa.etf.rpr.exceptions.MovieException;
 
+import java.util.List;
+
+public class MovieManager {
+    public List<Movie> getAll() throws MovieException {
+        return DaoFactory.movieDao().getAll();
+    }
+    public void delete(int id) throws MovieException {
+        DaoFactory.movieDao().delete(id);
+    }
+    public Movie add(Movie item) throws MovieException{
+        return DaoFactory.movieDao().add(item);
+    }
+    public Movie update(Movie item) throws MovieException{
+        return DaoFactory.movieDao().update(item);
+    }
 }
