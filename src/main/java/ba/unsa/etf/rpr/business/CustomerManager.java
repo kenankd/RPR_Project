be481473345f9017c09e8a3553e542a24e6c762a
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-12 20:42:54 +0100

    added getAll to CustomerManager

--- src/main/java/ba/unsa/etf/rpr/business/CustomerManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CustomerManager.java
@@ -1,8 +1,16 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Customer;
+import ba.unsa.etf.rpr.exceptions.MovieException;
+
+import java.util.List;
+
 /**
  * Business Logic Layer for management of Customers
  */
 public class CustomerManager {
-
+    public List<Customer> getAll() throws MovieException {
+        return DaoFactory.customerDao().getAll();
+    }
 }
