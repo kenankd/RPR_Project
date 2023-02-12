6150ea3d42de36c36a8aa32776ab93fcd126a0fa
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-12 20:43:43 +0100

    added delete to CustomerManager

--- src/main/java/ba/unsa/etf/rpr/business/CustomerManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CustomerManager.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Customer;
 import ba.unsa.etf.rpr.exceptions.MovieException;
@@ -12,5 +13,8 @@ import java.util.List;
 public class CustomerManager {
     public List<Customer> getAll() throws MovieException {
         return DaoFactory.customerDao().getAll();
+    }
+    public void delete(int id) throws MovieException {
+        DaoFactory.customerDao().delete(id);
     }
 }
