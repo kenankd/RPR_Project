68be87673953bfc39d4e0b8bbed7d41dadec2ac2
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-13 18:28:45 +0100

    renamed class

--- src/main/java/ba/unsa/etf/rpr/controllers/AlertDisplay.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AlertDisplay.java
@@ -2,7 +2,17 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.scene.control.Alert;
 
-public class ControllerHelper {
+/**
+ * @author Kenan Dizdarevic
+ * Class which is used for help, it displays an alert of given information
+ */
+public class AlertDisplay {
+    /**
+     * method which displays an alert on screen
+     * @param title of alert
+     * @param header of alert
+     * @param content of alert
+     */
     public static void showAlert(String title, String header, String content){
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
