f9952945422974df544878f46c8681e19cf69fc5
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-13 18:28:56 +0100

    added javadoc comments

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -17,6 +17,10 @@ import java.util.ResourceBundle;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
+/**
+ * @author Kenan Dizdarevic
+ * Controller class which controls everything behind the home display of the app
+ */
 public class HomeController{
     public MenuItem buttonMovies;
     public MenuItem buttonMyMovies;
@@ -24,6 +28,11 @@ public class HomeController{
 
     public Label labelWelcome;
 
+    /**
+     * method which switches the display from home to movies section
+     * @param actionEvent
+     * @throws IOException
+     */
     public void showMovies(ActionEvent actionEvent) throws IOException {
         Stage stage1=(Stage) menu.getScene().getWindow();
         FXMLLoader fxmlloader=new FXMLLoader(getClass().getResource("/fxml/movies.fxml"));
@@ -33,6 +42,11 @@ public class HomeController{
         stage1.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
     }
 
+    /**
+     * method which switches the display from home to mymovies section
+     * @param actionEvent
+     * @throws IOException
+     */
     public void showMyMovies(ActionEvent actionEvent) throws IOException {
         Stage stage1=(Stage) menu.getScene().getWindow();
         FXMLLoader fxmlloader=new FXMLLoader(getClass().getResource("/fxml/mymovies.fxml"));
@@ -41,6 +55,11 @@ public class HomeController{
         stage1.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
     }
 
+    /**
+     * method which logs out the user and goes back to the log in screen
+     * @param actionEvent
+     * @throws IOException
+     */
     public void showLogin(ActionEvent actionEvent) throws IOException {
         Stage stage = (Stage) menu.getScene().getWindow();
         Stage stage1 = new Stage();
