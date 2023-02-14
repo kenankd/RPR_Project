1ccea389707801f045de0840ea60d70de1f44bcb
Kenan <kdizdarevi1@etf.unsa.ba>
2023-02-14 16:00:58 +0100

    CLI FINISHED

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -17,6 +17,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Stream;
@@ -29,17 +30,23 @@ public class App {
     /**
      * Defining final variables to describe all code having options
      */
-    private static final Option addMovie = new Option("m","add-movie",false, "Adding new movie to database");
+    private static final Option addMovie = new Option("addM","add-movie",false, "Adding new movie to database, parameters should go in this order : title,date,actor,price,length,genre");
     //private static final Option buyMovie = new Option("bm","buy-movie",false, "Adding new purchase to  database");
     private static final Option getMovies = new Option("getM", "get-movie",false, "Printing all movies from database");
-    private static final Option getCustomers = new Option("getCust", "get-customers",false, "Printing all customers from database");
+    private static final Option getCustomers = new Option("getC", "get-customers",false, "Printing all customers from database");
     private static final Option addCustomer = new Option("addC", "add-customer",false, "Adding new customer to database");
     private static final Option nameDefinition = new Option(null, "name", false,"Defining name for customer");
     private static final Option surnameDefinition = new Option(null, "surname", false,"Defining surname for customer");
     private static final Option mailDefinition = new Option(null, "mail", false,"Defining mail for customer");
     private static final Option usernameDefinition = new Option(null, "username", false,"Defining username for customer");
     private static final Option pwDefinition = new Option(null, "pw", false,"Defining password for customer");
-  //  private static final Option priceDefinition = new Option(null, "price", false,"Defining price for nes added clothing");
+    private static final Option titleDefinition = new Option(null, "title", false,"Defining title of the movie");
+    private static final Option dateDefinition = new Option(null, "date", false,"Defining release date of the movie, enter in this format \"dd/MM/yyyy\"");
+    private static final Option actorDefinition = new Option(null, "actor", false,"Defining main actor of the movie");
+    private static final Option priceDefinition = new Option(null, "price", false,"Defining price of the movie");
+    private static final Option lengthDefinition = new Option(null, "length", false,"Defining length of the movie");
+    private static final Option genreDefinition = new Option(null, "genre", false,"Defining genre of the movie");
+
 
 
 
@@ -51,7 +58,7 @@ public class App {
     public static void printFormattedOptions (Options options) {
         HelpFormatter helpFormatter = new HelpFormatter();
         PrintWriter printWriter = new PrintWriter(System.out);
-        helpFormatter.printUsage(printWriter, 150, "java -jar Projekat-0.1.jar [option] 'something else if needed' ");
+        helpFormatter.printUsage(printWriter, 150, "java -jar Projekat-0.1-jar-with-dependencies.jar [option] 'something else if needed' ");
         helpFormatter.printOptions(printWriter, 150, options, 2, 7);
         printWriter.close();
     }
@@ -66,6 +73,12 @@ public class App {
         options.addOption(mailDefinition);
         options.addOption(usernameDefinition);
         options.addOption(pwDefinition);
+        options.addOption(titleDefinition);
+        options.addOption(dateDefinition);
+        options.addOption(actorDefinition);
+        options.addOption(priceDefinition);
+        options.addOption(lengthDefinition);
+        options.addOption(genreDefinition);
         return options;
     }
 
@@ -112,7 +125,23 @@ public class App {
                 System.out.println("Error, customer not added to database!");
             }
         }
-      //  else if((cl.hasOption(addMovie.getOpt()) || cl.hasOption(addMovie.getLongOpt())) && )
+        else if((cl.hasOption(addMovie.getOpt()) || cl.hasOption(addMovie.getLongOpt())) && cl.hasOption(titleDefinition.getLongOpt()) && cl.hasOption(dateDefinition.getLongOpt()) &&
+        cl.hasOption(actorDefinition.getLongOpt())&& cl.hasOption(priceDefinition.getLongOpt()) && cl.hasOption(lengthDefinition.getLongOpt()) && cl.hasOption(genreDefinition.getLongOpt())){
+            MovieManager movieManager = new MovieManager();
+            Movie movie = new Movie();
+            movie.setTitle(cl.getArgList().get(0));
+            movie.setRelease_date(new SimpleDateFormat("dd/MM/yyyy").parse(cl.getArgList().get(1)));
+            movie.setMain_actor(cl.getArgList().get(2));
+            movie.setPrice(Double.parseDouble(cl.getArgList().get(3)));
+            movie.setLength(Integer.parseInt(cl.getArgList().get(4)));
+            movie.setGenre(cl.getArgList().get(5));
+            try{
+                movieManager.add(movie);
+                System.out.println("Movie successfully added to database!");
+            } catch (MovieException e) {
+                System.out.println("Error, movie not added to database!");
+            }
+        }
         else{
             printFormattedOptions(options);
             System.exit(-1);
